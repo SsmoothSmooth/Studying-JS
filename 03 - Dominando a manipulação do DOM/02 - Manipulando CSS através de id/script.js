2194e15c52552a6b2cda44cf0916abe32e8459f5
José Wilson de Lima <josewilson791@gmail.com>
2023-02-04 20:14:27 -0300

    :sparkles: feat: Usando o DOM para manipular o CSS da página

---
+++ 03 - Dominando a manipulação do DOM/02 - Manipulando CSS através de id/script.js
@@ -0,0 +1,7 @@
+var el = document.getElementById('box');
+
+        el.style.width = "100px";
+        el.style.height = "100px";
+        el.style.backgroundColor = "red";
+        el.style.marginLeft = "45%";
+
