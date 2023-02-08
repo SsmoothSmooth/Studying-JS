b7b6d648031df5ce2cf3be5586390651bf3e5ad2
José Wilson de Lima <josewilson791@gmail.com>
2023-02-08 00:58:45 -0300

    :sparkles: feat: Usando o Async e Await

--- 04 - Javascript moderno/06 - Promisses, Async e Await/script.js
+++ 04 - Javascript moderno/06 - Promisses, Async e Await/script.js
@@ -1,19 +1,26 @@
 function testes(){
     return new Promise(function(resolve, reject){
+        setTimeout(function(){
+           
+            const erro = false;
+    
+            if(erro){
+                reject('Erro..!!.');
+            }else{
+                resolve("A promisse foi resolvida com sucesso !");
+            }
 
-        const erro = true;
+        },200)
 
-        if(erro){
-            reject('Erro..!!.');
-        }else{
-            resolve("A promisse foi resolvida com sucesso !");
-        }
 
     })
 }
 
-testes().then(function(res){
-    alert(res);
-}).catch(function(err){
-    alert(err);
-})
+async function testes2(){
+    await testes().then(function(res){
+        alert(res);
+    });
+    alert('Hi')
+}
+
+testes2();
