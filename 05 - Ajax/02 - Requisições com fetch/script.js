52759bccca92a48d888f467791ce47ad7d67e0f8
José Wilson de Lima <josewilson791@gmail.com>
2023-02-09 11:18:38 -0300

    :sparkles: feat: Criando um cadastro, usando Requisição via FETCH

--- 05 - Ajax/02 - Requisições com fetch/script.js
+++ 05 - Ajax/02 - Requisições com fetch/script.js
@@ -1,3 +1,14 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
+fetch('https://jsonplaceholder.typicode.com/posts',{
+    method: 'POST',
+    body: JSON.stringify({
+        title: 'SmoothSmooth',
+        body: 'Meu texto',
+        userId: 10
+    }),
+    headers: {
+        'Content-Type': 'application/json; charset=UTF-8'
+    }
+
+})
 .then(response => response.json())
 .then(json => console.log(json));
